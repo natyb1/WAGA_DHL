c1219a313ca26d840347d96e2178e336702143d8
Natnael <68988452+natyb1@users.noreply.github.com>
2023-11-09 16:35:39 +0300

    Front end updated with landing page and some picture and text remaning to be changed

--- resources/views/frontend/navbar.blade.php
+++ resources/views/frontend/navbar.blade.php
@@ -9,17 +9,17 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <div class="navbar-nav">
-                    <a href="index.html" class="nav-item nav-link active">Home</a>
-                    <a href="about.html" class="nav-item nav-link">About</a>
-                    <a href="service.html" class="nav-item nav-link">Services</a>
+                    <a href="#home" class="nav-item nav-link active">Home</a>
+                    <a href="#about" class="nav-item nav-link">About</a>
+                    <a href="#features" class="nav-item nav-link">Features</a>
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                         <div class="dropdown-menu bg-light m-0">
-                            <a href="feature.html" class="dropdown-item">Features</a>
-                            <a href="team.html" class="dropdown-item">Our Team</a>
-                            <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                            <a href="appoinment.html" class="dropdown-item">Appoinment</a>
-                            <a href="404.html" class="dropdown-item">404 Page</a>
+                            <a href="#services" class="dropdown-item">Services</a>
+                            <a href="#appointment" class="dropdown-item">Appoinment</a>
+                            <a href="#team" class="dropdown-item">Our Team</a>
+                            <a href="#testimonial" class="dropdown-item">Testimonial</a>
+                            <a href="#404" class="dropdown-item">404 Page</a>
                         </div>
                     </div>
                     <a href="contact.html" class="nav-item nav-link">Contact</a>
---
+++ resources/views/frontend/testimonial.blade.php
@@ -0,0 +1,52 @@
+<div class="container-fluid testimonial py-5" id="testimonial">
+    <div class="container pt-5">
+        <div class="row gy-5 gx-0">
+            <div class="col-lg-6 pe-lg-5 wow fadeIn" data-wow-delay="0.3s">
+                <h1 class="display-6 text-white mb-4">What Clients Say About Our Services!</h1>
+                <p class="text-white mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                    tellus augue, iaculis id elit eget, ultrices pulvinar tortor.</p>
+                <a href="" class="btn btn-primary py-3 px-5">More Testimonials</a>
+            </div>
+            <div class="col-lg-6 mb-n5 wow fadeIn" data-wow-delay="0.5s">
+                <div class="bg-white p-5">
+                    <div class="owl-carousel testimonial-carousel wow fadeIn" data-wow-delay="0.1s">
+                        <div class="testimonial-item">
+                            <div class="icon-box-primary mb-4">
+                                <i class="bi bi-chat-left-quote text-dark"></i>
+                            </div>
+                            <p class="fs-5 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                                tellus augue, iaculis id elit eget, ultrices pulvinar tortor. Quisque vel lorem
+                                porttitor, malesuada arcu quis, fringilla risus. Pellentesque eu consequat augue.
+                            </p>
+                            <div class="d-flex align-items-center">
+                                <img class="flex-shrink-0" src="{{ asset('frontend/assets/img/testimonial-1.jpg') }}"
+                                    alt="">
+                                <div class="ps-3">
+                                    <h5 class="mb-1">Client Name</h5>
+                                    <span class="text-primary">Profession</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="testimonial-item">
+                            <div class="icon-box-primary mb-4">
+                                <i class="bi bi-chat-left-quote text-dark"></i>
+                            </div>
+                            <p class="fs-5 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                                tellus augue, iaculis id elit eget, ultrices pulvinar tortor. Quisque vel lorem
+                                porttitor, malesuada arcu quis, fringilla risus. Pellentesque eu consequat augue.
+                            </p>
+                            <div class="d-flex align-items-center">
+                                <img class="flex-shrink-0" src="{{ asset('frontend/assets/img/testimonial-2.jpg') }}"
+                                    alt="">
+                                <div class="ps-3">
+                                    <h5 class="mb-1">Client Name</h5>
+                                    <span class="text-primary">Profession</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
